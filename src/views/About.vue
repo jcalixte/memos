c6f6de6ba5d077312a48bb8fc6d9c3be0c07518d
Julien Calixte <julienc@bam.tech>
2020-03-18 23:32:07 +0100

    :bento: (logo) add logo to project

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
     <h1>Ceci est une page à propos.</h1>
+    <p>
+      Logo depuis
+      <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+        Freepik
+      </a>
+      fait par
+      <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+    </p>
   </div>
 </template>
